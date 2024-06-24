15180e61ff361e3a9a5da694b77de50ceb1adb36
Nikita <akknel3@gmail.com>
2024-06-24 18:00:00 +0300

    added header

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Sign in form modal window
+function openForm1() {
+  document.getElementById("sign_in-modal").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("sign_in-modal").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+// Sign up form modal window
+function openForm2() {
+  document.getElementById("sign_up-modal").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeForm2() {
+  document.getElementById("sign_up-modal").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
